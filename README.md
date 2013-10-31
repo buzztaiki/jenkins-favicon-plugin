0f43ee5071e8c37d73c76b6165a34215a0472ff1
Taiki Sugawara <buzz.taiki@gmail.com>
2013-11-01 02:30:17 +0900

    initial

---
+++ README.md
@@ -0,0 +1,10 @@
+Jenkins Favicon Plugin
+======================
+
+This plugin allows you to set a favicon to your jenkins.
+
+
+TODO
+----
+
+- use [favico](https://github.com/ejci/favico.js)?
---
+++ src/main/java/org/jenkinsci/plugins/favicon/FaviconPageDecorator.java
@@ -0,0 +1,11 @@
+package org.jenkinsci.plugins.favicon;
+
+import hudson.model.PageDecorator;
+import hudson.Extension;
+
+@Extension
+public final class FaviconPageDecorator extends PageDecorator {
+    public FaviconPageDecorator() {
+        super();
+    }
+}
