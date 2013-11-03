6b18d1c29a8138dfedc104ea8710349d4c88f751
Taiki Sugawara <buzz.taiki@gmail.com>
2013-11-04 01:50:47 +0900

    To configurable

--- src/main/java/org/jenkinsci/plugins/favicon/FaviconPageDecorator.java
+++ src/main/java/org/jenkinsci/plugins/favicon/FaviconPageDecorator.java
@@ -1,11 +1,38 @@
 package org.jenkinsci.plugins.favicon;
 
-import hudson.model.PageDecorator;
 import hudson.Extension;
+import hudson.model.PageDecorator;
+import net.sf.json.JSONObject;
+import org.kohsuke.stapler.StaplerRequest;
 
 @Extension
 public final class FaviconPageDecorator extends PageDecorator {
+    private String faviconPath;
+
     public FaviconPageDecorator() {
         super();
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "Favicon";
+    }
+
+    @Override
+    public boolean configure(StaplerRequest req, JSONObject json) throws FormException {
+        faviconPath = json.getString("faviconPath");
+        save();
+        return super.configure(req, json);
+    }
+
+    public String getFaviconPath() {
+        return faviconPath;
+    }
+
+    public String getJsFaviconPath() {
+        if (faviconPath == null) {
+            return null;
+        }
+        return faviconPath.replaceAll("['\"]", "\\\\$0");
     }
 }
